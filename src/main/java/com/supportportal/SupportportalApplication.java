58d51c81d91892597d8b7f56d96eb74ca4803bad
Alejandro Depetris <aledepetris@outlook.com>
2023-08-30 14:33:19 -0300

    Log main when app starts.

--- src/main/java/com/supportportal/SupportportalApplication.java
+++ src/main/java/com/supportportal/SupportportalApplication.java
@@ -1,13 +1,18 @@
 package com.supportportal;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Log4j2
 public class SupportportalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SupportportalApplication.class, args);
+		log.info("===========================================");
+		log.info("========  Support Portal RUNNING  ========");
+		log.info("===========================================");
 	}
 
 }
