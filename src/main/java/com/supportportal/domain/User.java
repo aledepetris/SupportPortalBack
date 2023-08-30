591c28f02bd24d0abfb8fc608566168c85139cd8
Alejandro Depetris <aledepetris@outlook.com>
2023-08-30 12:55:25 -0300

    Creation of User entity

---
+++ src/main/java/com/supportportal/domain/User.java
@@ -0,0 +1,32 @@
+package com.supportportal.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Data
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private String email;
+    private String profileImageUrl;
+    private Date lastLoginDate;
+    private Date lastLoginDateDisplay;
+    private Date joinDate;
+    private String[] roles; // ROLE_USER, ROLE_ADMIN
+    private String[] authorities;
+    private boolean isActive;
+    private boolean isNotLocked;
+
+}
