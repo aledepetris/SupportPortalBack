e2232f5baaa75c55e16fe38b1288411878aa7019
Alejandro Depetris <aledepetris@outlook.com>
2023-09-06 18:36:33 -0300

    Log and documentation for JWTTokenProvider

--- src/main/java/com/supportportal/utility/JWTTokenProvider.java
+++ src/main/java/com/supportportal/utility/JWTTokenProvider.java
@@ -8,6 +8,7 @@ import static java.util.Arrays.stream;
 
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.supportportal.domain.UserPrincipal;
+import lombok.extern.log4j.Log4j2;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -24,11 +25,18 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
+@Log4j2
 public class JWTTokenProvider {
 
-    @Value("jwt.secret")
+    @Value("${jwt.secret}")
     private String secret;
 
+    /**
+     * Generate a JWT token for a given UserPrincipal.
+     *
+     * @param userPrincipal The UserPrincipal for whom the token is generated.
+     * @return The generated JWT token.
+     */
     public String generateJwtToken(UserPrincipal userPrincipal) {
         String[] claims = getClaimFromUser(userPrincipal);
         return JWT.create()
@@ -41,6 +49,12 @@ public class JWTTokenProvider {
                 .sign(Algorithm.HMAC512(secret.getBytes()));
     }
 
+    /**
+     * Get a list of GrantedAuthority objects from a JWT token.
+     *
+     * @param token The JWT token.
+     * @return A list of GrantedAuthority objects.
+     */
     public List<GrantedAuthority> getAuthorities(String token) {
         String[] claims = getClaimFromToken(token);
         return stream(claims)
@@ -48,6 +62,14 @@ public class JWTTokenProvider {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Get Authentication object from a username, list of authorities, and an HTTP request.
+     *
+     * @param username    The username.
+     * @param authorities List of authorities.
+     * @param request     The HTTP request.
+     * @return The Authentication object.
+     */
     public Authentication getAuthentication(String username, List<GrantedAuthority> authorities, HttpServletRequest request) {
         UsernamePasswordAuthenticationToken userPasswordAuthToken
                 = new UsernamePasswordAuthenticationToken(username, null, authorities);
@@ -56,11 +78,30 @@ public class JWTTokenProvider {
         return userPasswordAuthToken;
     }
 
+    /**
+     * Check if a JWT token is valid for a given username.
+     *
+     * @param username The username.
+     * @param token    The JWT token.
+     * @return True if the token is valid, false otherwise.
+     */
     public boolean isTokenValid(String username, String token) {
         JWTVerifier verifier = getJWTVerifier();
-        return StringUtils.isNotEmpty(username) && isTokenExpired(verifier, token);
+        boolean isValid = StringUtils.isNotEmpty(username) && !isTokenExpired(verifier, token);
+
+        if (!isValid) {
+            log.error("JWT token for user {} is invalid or expired.", username);
+        }
+
+        return isValid;
     }
 
+    /**
+     * Get the subject (username) from a JWT token.
+     *
+     * @param token The JWT token.
+     * @return The subject (username).
+     */
     public String getSubject(String token) {
         JWTVerifier verifier = getJWTVerifier();
         return verifier.verify(token).getSubject();
