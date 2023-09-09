d6c4560f4bfd4afb75d2011682ec04cb56e63a0e
Alejandro Depetris <aledepetris@outlook.com>
2023-09-08 23:32:52 -0300

    Mino changes in User to storage onle a role instead an array of roles. Creation of Authorities and Roles

---
+++ src/main/java/com/supportportal/constant/Authority.java
@@ -0,0 +1,22 @@
+package com.supportportal.constant;
+
+public class Authority {
+
+    private Authority() {
+        throw new IllegalStateException("Utility class");
+    }
+
+
+    private static final String USER_READ = "user:read";
+    private static final String USER_UPDATE = "user:update";
+    private static final String USER_CREATE = "user:create";
+    private static final String USER_DELETE = "user:delete";
+
+
+    public static final String[] USER_AUTHORITIES = { USER_READ };
+    public static final String[] HR_AUTHORITIES = { USER_READ, USER_UPDATE };
+    public static final String[] MANAGER_AUTHORITIES = { USER_READ, USER_UPDATE };
+    public static final String[] ADMIN_AUTHORITIES = { USER_READ, USER_UPDATE, USER_CREATE };
+    public static final String[] SUPER_USER_AUTHORITIES = {USER_READ, USER_UPDATE, USER_CREATE, USER_DELETE };
+
+}
---
+++ src/main/java/com/supportportal/enumaration/Role.java
@@ -0,0 +1,23 @@
+package com.supportportal.enumaration;
+
+import static com.supportportal.constant.Authority.*;
+
+public enum Role {
+
+    ROLE_USER(USER_AUTHORITIES),
+    ROLE_HR(HR_AUTHORITIES),
+    ROLE_MANAGER(MANAGER_AUTHORITIES),
+    ROLE_ADMIN(ADMIN_AUTHORITIES),
+    ROLE_SUPER_ADMIN(SUPER_USER_AUTHORITIES);
+
+    private final String[] authorities;
+
+    Role(String... authorities) {
+        this.authorities = authorities;
+    }
+
+    public String[] getAuthorities() {
+        return authorities;
+    }
+
+}
