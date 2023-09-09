3da4d9e18375cfe1d8cdad787475ffd2abb15ae5
Alejandro Depetris <aledepetris@outlook.com>
2023-09-08 21:51:28 -0300

    Adding timestamp to HttpResponse

--- src/main/java/com/supportportal/domain/HttpResponse.java
+++ src/main/java/com/supportportal/domain/HttpResponse.java
@@ -1,8 +1,10 @@
 package com.supportportal.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 import org.springframework.http.HttpStatus;
 
+import java.util.Date;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -10,6 +12,9 @@ import org.springframework.http.HttpStatus;
 @Getter @Setter
 public class HttpResponse {
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    @Builder.Default
+    private Date timeStamp = new Date();
     private int httpStatusCode;
     private HttpStatus httpStatus;
     private String reason;
