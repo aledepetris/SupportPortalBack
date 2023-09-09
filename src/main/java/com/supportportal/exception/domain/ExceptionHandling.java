df0a3a7d2c8ae1679461618651fb0c5616068938
Alejandro Depetris <aledepetris@outlook.com>
2023-09-08 22:57:48 -0300

    Remove default white label page for not found 404 results.

--- src/main/java/com/supportportal/exception/domain/ExceptionHandling.java
+++ src/main/java/com/supportportal/exception/domain/ExceptionHandling.java
@@ -5,6 +5,7 @@ import com.supportportal.domain.HttpResponse;
 import lombok.extern.slf4j.Slf4j;
 import static org.springframework.http.HttpStatus.*;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +14,9 @@ import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.LockedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import javax.persistence.NoResultException;
 import java.io.IOException;
@@ -21,9 +24,10 @@ import java.nio.file.AccessDeniedException;
 import java.util.Arrays;
 import java.util.Objects;
 
-@RestControllerAdvice
 @Slf4j
-public class ExceptionHandling {
+@RestControllerAdvice
+@SuppressWarnings("all")
+public class ExceptionHandling implements ErrorController {
 
     public static final String ACCOUNT_LOCKED = "Your account has been locked. Please contact administration";
     public static final String METHOD_IS_NOT_ALLOWED = "This request method is not allowed on this endpoint. Please send a '%s' request";
@@ -32,6 +36,7 @@ public class ExceptionHandling {
     public static final String ACCOUNT_DISABLED = "Your account has been disables. If this is an error, please contact administration";
     public static final String ERROR_PROCESSING_FILE = "Error occurred while processing file";
     public static final String NOT_ENOUGH_PERMISSION = "You do not have enough permission";
+    public static final String ERROR_PATH = "/error";
 
     @ExceptionHandler(DisabledException.class)
     public ResponseEntity<HttpResponse> accountDisabledException() {
@@ -78,6 +83,12 @@ public class ExceptionHandling {
         return createHttpResponse(BAD_REQUEST, ex.getMessage());
     }
 
+//  ** A way to remove white label spring page for not found NOT RECOMENDED
+//    @ExceptionHandler(NoHandlerFoundException.class)
+//    public ResponseEntity<HttpResponse> noHandlerFoundException(HttpRequestMethodNotSupportedException ex) {
+//        return createHttpResponse(BAD_REQUEST, "This page was not found");
+//    }
+
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public ResponseEntity<HttpResponse> userNotFoundException(HttpRequestMethodNotSupportedException ex) {
         HttpMethod supportedMethod = Objects.requireNonNull(ex.getSupportedHttpMethods()).iterator().next();
@@ -115,4 +126,13 @@ public class ExceptionHandling {
 
     }
 
+    @Override
+    public String getErrorPath() {
+        return ERROR_PATH;
+    }
+
+    @RequestMapping(ERROR_PATH)
+    public ResponseEntity<HttpResponse> notFound404(Exception ex) {
+        return createHttpResponse(NOT_FOUND, "There is no mapping for this URL");
+    }
 }
