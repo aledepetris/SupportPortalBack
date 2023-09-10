523827010bc94a4bb6ad4a1396267a0f14c76c8f
Alejandro Depetris <aledepetris@outlook.com>
2023-09-10 18:47:27 -0300

    Creation for user folder when app run.

--- src/main/java/com/supportportal/SupportportalApplication.java
+++ src/main/java/com/supportportal/SupportportalApplication.java
@@ -6,12 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.io.File;
+
+import static com.supportportal.constant.FileConstant.USER_FOLDER;
+
 @SpringBootApplication
 @Log4j2
 public class SupportportalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SupportportalApplication.class, args);
+		new File(USER_FOLDER).mkdirs();
 		log.info("===========================================");
 		log.info("========  Support Portal RUNNING  ========");
 		log.info("===========================================");
